6b81e7aa6ccc31c67c3003aad1a69992316079c2
danderson00 <danderson00@gmail.com>
2017-11-27 14:19:31 +1100

    Add worker tests

---
+++ test/worker.test.js
@@ -0,0 +1,56 @@
+var stub = require('../src/worker').stub
+var host = require('../src/host')
+
+test("an object client is returned for modules that export an API", () => {
+  return host(stub({ hello: () => 'world' }))
+    .then(api => {
+      expect(Object.keys(api)).toEqual(['hello', 'terminate'])
+      return api.hello()
+    })
+    .then(result => expect(result).toBe('world'))
+})
+
+test("a function client is returned for modules that export a function", () => {
+  return host(stub(() => 'world' ))
+    .then(api => api())
+    .then(result => expect(result).toBe('world'))
+})
+
+test("initialisation is expected for an API worker when a parameter is supplied", () => {
+  return host(stub(param => ({ hello: () => param + ' world' })), { parameter: 'hello' })
+    .then(api => {
+      expect(Object.keys(api)).toEqual(['hello', 'terminate'])
+      return api.hello()
+    })
+    .then(result => expect(result).toBe('hello world'))
+})
+
+test("initialisation is expected for a function worker when a parameter is supplied", () => {
+  return host(stub(param => () => param + ' world'), { parameter: 'hello' })
+    .then(api => api())
+    .then(result => expect(result).toBe('hello world'))
+})
+
+test("initialisation is expected for an API worker when includeEmit is specified", () => {
+  return host(stub(emit => ({ hello: () => 'world' })), { includeEmit: true })
+    .then(api => {
+      expect(Object.keys(api)).toEqual(['hello', 'terminate'])
+      return api.hello()
+    })
+    .then(result => expect(result).toBe('world'))
+})
+
+test("initialisation is expected for a function worker when includeEmit is specified", () => {
+  return host(stub(emit => () => 'world'), { includeEmit: true })
+    .then(api => api())
+    .then(result => expect(result).toBe('world'))
+})
+
+test("parameter is the first initialisation argument when both parameter and includeEmit are specified", () => {
+  return host(stub((param, emit) => ({ hello: () => param + ' world' })), { parameter: 'hello', includeEmit: true })
+    .then(api => {
+      expect(Object.keys(api)).toEqual(['hello', 'terminate'])
+      return api.hello()
+    })
+    .then(result => expect(result).toBe('hello world'))
+})
